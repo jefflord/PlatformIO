03d13b400e6e63efa4e28cb32013ba9944af9381
Jeff Lord <jeff@jefflord.com>
2024-09-04 12:43:21 -0400

    auto-build

--- Projects/SSD1331/src/main.cpp
+++ Projects/SSD1331/src/main.cpp
@@ -292,9 +292,17 @@ int touchValue = 0;
 
 void pushServoButton()
 {
-  myServo.write(150);
-  delay(500);
+  myServo.write(0);
+  delay(1000);
+
   myServo.write(90);
+  delay(1000);
+
+  myServo.write(180);
+  delay(1000);
+
+  myServo.write(0);
+  delay(1000);  
 }
 
 void loop()
