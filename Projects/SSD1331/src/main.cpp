2093d9d323078b47de7cf89e8ea266c791d0e7cb
Jeff Lord <jeff@jefflord.com>
2024-09-04 19:17:24 -0400

    auto-build

--- Projects/SSD1331/src/main.cpp
+++ Projects/SSD1331/src/main.cpp
@@ -378,7 +378,7 @@ void updateDisplay(void *p)
 
     char timeString[6]; // Buffer for formatted time
     // sprintf(timeString, "%02d:%02d:%02d %s", hours, minutes, seconds, ampm.c_str());
-    sprintf(timeString, "%4.1f", temperatureC);
+    sprintf(timeString, "%4.1f°", temperatureC);
 
     gfx->print(timeString);
 
